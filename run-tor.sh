0cd8e2ff501f8f808af7df8cac2a46ad78670924
bpawnzZ <bpawnz@yandex.com>
2024-09-15 07:57:14 -0400

    final commit

--- run-tor.sh
+++ run-tor.sh
@@ -10,10 +10,15 @@ SocksPort $TOR_SOCKS_PORT
 ControlPort 9051
 RelayBandwidthRate $RELAY_BANDWIDTH_RATE KB
 RelayBandwidthBurst $RELAY_BANDWIDTH_BURST KB
+Address $EXTERNAL_ADDRESS
+IPv6Exit $IPV6_EXIT
+ClientUseIPv4 1
+ClientUseIPv6 0
+ClientPreferIPv6ORPort 0
 EOT
 
 if [ "$RELAY_TYPE" = "GUARD" ]; then
     echo "GuardLifetime 2 months" >> /etc/tor/torrc
 fi
 
-exec tor -f /etc/tor/torrc
+exec tor -f /etc/tor/torrc 
